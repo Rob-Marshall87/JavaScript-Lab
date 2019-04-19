7bb6df7d0d1e386005ef5c6d8710a5201d450067
Mark Mackay <Mark_Mackay@yahoo.com>
2019-04-19 18:56:14 +0100

    added app.js, model files and view file placeholders

---
+++ client/src/app.js
@@ -0,0 +1,19 @@
+const FilmTriviaFormView = require('./views/film_trivia_form_view.js')
+const FilmTriviaGridView = require('./views/film_trivia_grid_view.js');
+const FilmTrivia = require('./models/film_trivia.js');
+const Grid = require('./models/grid.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const filmTriviaForm = document.querySelector('form#filmTrivia');
+  const filmTriviaFormView = new FilmTriviaFormView(filmTriviaForm);
+  filmTriviaFormView.bindEvents();
+
+  const filmTriviaGridContainer = document.querySelector('div#filmTrivia');
+  const filmTriviaGridView = new FilmTriviaGridView(filmTriviaGridContainer);
+  filmTriviaGridView.bindEvents();
+
+  const url = 'http://localhost:3000/api/questions';
+  const filmTrivia = new FilmTrivia(url);
+  filmTrivia.bindEvents();
+  filmTrivia.getData();
+});
---
+++ client/src/models/film_trivia.js
@@ -0,0 +1,11 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const FilmTrivia = function (url) {
+  this.url = url;
+  this.request = new RequestHelper(this.url);
+};
+
+FilmTrivia.prototype.bindEvents = function () {
+
+};
