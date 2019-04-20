7997489534ab27322f5ae075c60021f1c3ccc04b
Mark Mackay <Mark_Mackay@yahoo.com>
2019-04-20 15:43:38 +0100

    stuck on how to get random box to jump around

--- client/src/app.js
+++ client/src/app.js
@@ -4,16 +4,19 @@ const FilmTrivia = require('./models/film_trivia.js');
 const Grid = require('./models/grid.js');
 
 document.addEventListener('DOMContentLoaded', () => {
-  const filmTriviaForm = document.querySelector('form#filmTrivia');
-  const filmTriviaFormView = new FilmTriviaFormView(filmTriviaForm);
-  filmTriviaFormView.bindEvents();
+  // const filmTriviaForm = document.querySelector('form#filmTrivia');
+  // const filmTriviaFormView = new FilmTriviaFormView(filmTriviaForm);
+  // filmTriviaFormView.bindEvents();
+  //
+  // const filmTriviaGridContainer = document.querySelector('div#filmTrivia');
+  // const filmTriviaGridView = new FilmTriviaGridView(filmTriviaGridContainer);
+  // filmTriviaGridView.bindEvents();
 
-  const filmTriviaGridContainer = document.querySelector('div#filmTrivia');
-  const filmTriviaGridView = new FilmTriviaGridView(filmTriviaGridContainer);
-  filmTriviaGridView.bindEvents();
-
-  const url = 'http://localhost:3000/api/questions';
-  const filmTrivia = new FilmTrivia(url);
-  filmTrivia.bindEvents();
-  filmTrivia.getData();
+  // const url = 'http://localhost:3000/api/questions';
+  // const filmTrivia = new FilmTrivia(url);
+  // filmTrivia.bindEvents();
+  // filmTrivia.getData();
+  const grid = new Grid();
+  grid.populate();
+  grid.randomiser();
 });
--- client/src/models/grid.js
+++ client/src/models/grid.js
@@ -1,9 +1,26 @@
+const PubSub = require('../helpers/pub_sub.js');
 const Grid = function(){
-  this.grid = [false, false, false,
-               false, false, false,
-               false, true, false]
+  this.grid = [true, false, false,
+               false, true, false,
+               false, false, true]
 }
+// Grid.prototype.bindEvents = funtion() {
+//   PubSub.subscribe('FilmTrivia: Randomiser-clicked', () => {
+//
+//   })
+// }
 
+// Grid.prototype.createRandomiserButton = function () {
+//   const button = document.createElement('button');
+//   button.classList.add('random-btn');
+//   button.value = blah;
+//
+//   button.addEventListener('click', (evt) => {
+//     PubSub.publish('Grid: Randomiser-clicked', evt.target.value);
+//   });
+//
+//   return button;
+// };
 Grid.prototype.populate = function() {
     for (i = 0; i < 9; i++) {
       if (this.grid[i]) {
@@ -20,10 +37,45 @@ Grid.prototype.reset = function() {
     this.populate();
 }
 Grid.prototype.randomiser = function() {
-    // for (i = 0; i < 9; i++) {
-    //   this.grid[i] = false;
-    // }
-    // this.populate();
+    const active = [];
+      let box;
+    for (i = 0; i < 9; i++) {
+      if (this.grid[i] === false) {
+        active.push(i);
+        box = document.querySelector(`#grid-${i}`);
+        setInterval(function () {
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            box.style.backgroundColor = "#"+randomColor;
+        },100); };
+    }
+    console.log(active);
+    // console.log('Active:', active);
+    // let index = 0;
+    //
+    // let colour = 'rgba(128, 128, 255, 1.0)'
+    //   console.log(colour);
+    // // while (true) {
+    //   for (let x = 0; x < 20; x++){
+    //     index =  this.randomIndex(active.length);
+    //       // console.log(index);
+    //     box = document.querySelector(`#grid-${active[index]}`);
+    //     setInterval(function () {
+    //         var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    //         box.style.backgroundColor = "#"+randomColor;
+    //     },1000);
+    //     // (colour === 'red') ? colour = 'rgba(128, 128, 255, 1.0)': colour = 'red';
+    //     // setInterval(this.goColour(box, colour), 250);
+    //   }
+}
+Grid.prototype.goColour = function(box, colour){
+  box.style.backgroundColor = colour;
+};
+Grid.prototype.randomIndex = function(number){
+  return Math.floor(Math.random() * (number));
+}
+
+Grid.prototype.flash = function(box, colour){
+  box.style.backgroundColor = colour;
 }
 
 module.exports = Grid;
