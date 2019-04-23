fcd1187b84789d8f4c2413d08b00e3fa9ca9576f
Mark Mackay <Mark_Mackay@yahoo.com>
2019-04-23 12:09:04 +0100

    Did some more grid logic

--- client/src/models/grid.js
+++ client/src/models/grid.js
@@ -2,13 +2,43 @@ const PubSub = require('../helpers/pub_sub.js');
 const Grid = function(){
   this.grid = [true, false, false,
                false, true, false,
-               false, false, true]
+               false, false, true];
+  this.answers = [{film:'seven', file: '0.png' },
+          { film: 'pulp fiction', file:'1.png' },
+          { film:'blade runner 2049', file:'2.jpg' },
+          { film:'raiders of the lost ark', file:'3.png' },
+          { film:'lord of the rings', file:'4.jpg' } ,
+          { film:'inglorious bastards', file:'5.png' },
+          { film:'empire strikes back', file:'6.png' },
+          { film:'alien', file:'7.png' },
+          { film:'skyfall', file:'8.png' },
+          { film:'temple of doom', file:'9.png' },
+          { film:'rogue one', file:'10.jpg' },
+          { film:'sin city', file:'11.jpg' },
+          { film: 'psycho', file: '12.png' },
+          { film: 'ghostbusters 2', file: '13.png' },
+          { film: 'the truman show', file: '14.png' },
+          { film: 'kill bill volume 1', file: '15.png' },
+          { film: 'the shining', file: '16.jpg' },
+          { film: 'ghostbusters', file: '17.jpg' },
+          { film: 'star wars', file: '18.png' },
+          { film: 'lord of the rings', file: '19.png' }
+        ],
+        this.currentFilm = this.selectRandomFilm();
+        this.gameOver = false;
+
 }
-// Grid.prototype.bindEvents = funtion() {
-//   PubSub.subscribe('FilmTrivia: Randomiser-clicked', () => {
-//
-//   })
-// }
+
+Grid.prototype.bindEvents = function() {
+
+  PubSub.subscribe('FilmTrivia: Question-Answered', (evt) => {
+    if (evt.detail.toLowerCase() === currentFilm.film ) {
+      this.clearGrid();
+      this.gameOver = true;
+    }
+  });
+  console.log(this.currentFilm);
+}
 
 // Grid.prototype.createRandomiserButton = function () {
 //   const button = document.createElement('button');
@@ -21,6 +51,10 @@ const Grid = function(){
 //
 //   return button;
 // };
+Grid.prototype.selectRandomFilm = function(){
+  filmIndex = this.randomIndex(answers.length);
+  return this.answers.splice(filmIndex, 1);
+}
 Grid.prototype.populate = function() {
     for (i = 0; i < 9; i++) {
       if (this.grid[i]) {
@@ -31,7 +65,7 @@ Grid.prototype.populate = function() {
     }
 }
 Grid.prototype.reset = function() {
-    for (i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
       this.grid[i] = false;
     }
     this.populate();
@@ -48,11 +82,14 @@ Grid.prototype.randomiser = function() {
     let colour = 'rgba(128, 128, 255, 1.0)'
 
     index =  this.randomIndex(active.length);
-    this.tenTimes(active, active.length, 6).then(console.log(index));
-
-
-
+    this.tenTimes(active, active.length, 6);
 }
+Grid.prototype.clearGrid = function() {
+  for (let i = 0; i < 9; i++) {
+        const box = document.querySelector(`#grid-${i}`);
+        box.style.opacity = '0.0';
+    };
+  };
 
 Grid.prototype.tenTimes = function(active, len, count){
   this.changeBox(active, len);
