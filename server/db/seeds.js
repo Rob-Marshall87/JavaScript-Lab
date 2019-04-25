8f5347386e23f52449a5807a908d158aa1635d65
Rob-Marshall87 <marshallrobbie@hotmail.com>
2019-04-25 10:32:04 +0100

    new questions added

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -17,7 +17,7 @@ db.questions.insertMany([{
   category: "Entertainment: Film",
   type: "multiple",
   difficulty: "easy",
-  question: "Which of the following is not the name of a 'Bond Girl'? ",
+  questions: "Which of the following is not the name of a 'Bond Girl'? ",
   correct_answer: "Vanessa Kensington",
   incorrect_answer: [
     "Pam Bouvier",
@@ -178,7 +178,7 @@ db.questions.insertMany([{
   incorrect_answer: [
     "Jackie Chan",
     "Jet Li",
-    "Yun-Fat Chow"
+    " Yun-Fat Chow"
   ]
 },
 {
@@ -210,7 +210,7 @@ db.questions.insertMany([{
   type: "multiple",
   difficulty: "easy",
   question: "Who directed the 2015 movie 'The Revenant'?",
-  correct_answer: "Alejandro G. I&ntilde;&aacute;rritu",
+  correct_answer: "Alejandro G. I&ntilde;ârritu",
   incorrect_answer: [
     "Christopher Nolan",
     "David Fincher",
@@ -600,6 +600,653 @@ db.questions.insertMany([{
     "Higgins Haven",
     "Camp Forest Green"
   ]
-}
-
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "easy",
+question: "Who starred as Bruce Wayne and Batman in Tim Burton&#039;s 1989 movie &quot;Batman&quot;?",
+correct_answer: "Michael Keaton",
+incorrect_answer: [
+"George Clooney",
+"Val Kilmer",
+"Adam West"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "easy",
+question: "Who starred in the film 1973 movie &quot;Enter The Dragon&quot;?",
+correct_answer: "Bruce Lee",
+incorrect_answer: [
+"Jackie Chan",
+"Jet Li",
+" Yun-Fat Chow"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "easy",
+question: "What was Dorothy&#039;s surname in &#039;The Wizard Of Oz&#039;?",
+correct_answer: "Gale",
+incorrect_answer: [
+"Perkins",
+"Day",
+"Parker"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "easy",
+question: "This movie contains the quote, &quot;Houston, we have a problem.&quot;",
+correct_answer: "Apollo 13",
+incorrect_answer: [
+"The Right Stuff",
+"Capricorn One",
+"Marooned"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "easy",
+question: "What was the first monster to appear alongside Godzilla?",
+correct_answer: "Anguirus",
+incorrect_answer: [
+"King Kong",
+"Mothra",
+"King Ghidora"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Which of the following is NOT a quote from the 1942 film Casablanca? ",
+correct_answer: "&quot;Frankly, my dear, I don&#039;t give a damn.&quot;",
+incorrect_answer: [
+"&quot;Here&#039;s lookin&#039; at you, kid.&quot;",
+"&ldquo;Of all the gin joints, in all the towns, in all the world, she walks into mine&hellip;&rdquo;",
+"&quot;Round up the usual suspects.&quot;"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In the movie &ldquo;The Iron Giant,&rdquo; this character is the protagonist.",
+correct_answer: "Hogarth Hughes",
+incorrect_answer: [
+"Kent Mansley",
+"Dean McCoppin",
+"Annie Hughes"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What was Marilyn Monroe`s character&#039;s first name in the film &quot;Some Like It Hot&quot;?",
+correct_answer: "Sugar",
+incorrect_answer: [
+"Honey",
+"Caramel",
+"Candy"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In which 1973 film does Yul Brynner play a robotic cowboy who malfunctions and goes on a killing\tspree?",
+correct_answer: "Westworld",
+incorrect_answer: [
+"Runaway",
+"Android",
+"The Terminators"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who voiced the character Draco in the 1996 movie &#039;DragonHeart&#039;?",
+correct_answer: "Sean Connery",
+incorrect_answer: [
+"Dennis Quaid",
+"Pete Postlethwaite",
+"Brian Thompson"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In the 1984 movie &quot;The Terminator&quot;, what model number is the Terminator portrayed by Arnold Schwarzenegger?",
+correct_answer: "T-800",
+incorrect_answer: [
+"I-950",
+"T-888",
+"T-1000"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What is the name of James Dean&#039;s character in the 1955 movie &quot;Rebel Without a Cause&quot;?",
+correct_answer: "Jim Stark",
+incorrect_answer: [
+"Ned Stark",
+"Jim Kane",
+"Frank Stark"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In which 1955 film does Frank Sinatra play Nathan Detroit?",
+correct_answer: "Guys and Dolls",
+incorrect_answer: [
+"Anchors Aweigh",
+"From Here to Eternity",
+"High Society"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What is the name of the robot in the 1951 science fiction film classic &#039;The Day the Earth Stood Still&#039;?",
+correct_answer: "Gort",
+incorrect_answer: [
+"Robby",
+"Colossus",
+"Box"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Which of the following James Bond villains is not affiliated with the SPECTRE organization?",
+correct_answer: "Auric Goldfinger",
+incorrect_answer: [
+"Dr. Julius No",
+"Rosa Klebb",
+"Emilio Largo"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who played Batman in the 1997 film &quot;Batman and Robin&quot;?",
+correct_answer: "George Clooney",
+incorrect_answer: [
+"Michael Keaton",
+"Val Kilmer",
+"Christian Bale"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "This movie contains the quote, &quot;I love the smell of napalm in the morning!&quot;",
+correct_answer: "Apocalypse Now",
+incorrect_answer: [
+"Platoon",
+"The Deer Hunter",
+"Full Metal Jacket"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "This movie contains the quote, &quot;What we&#039;ve got here is a failure to communicate.&quot;",
+correct_answer: "Cool Hand Luke",
+incorrect_answer: [
+"Bonnie and Clyde",
+"The Graduate",
+"In the Heat of the Night"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In which African country was the 2006 film &#039;Blood Diamond&#039; mostly set in?",
+correct_answer: "Sierra Leone",
+incorrect_answer: [
+"Liberia",
+"Burkina Faso",
+"Central African Republic"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In what year was the first Transformers movie released?",
+correct_answer: "1986",
+incorrect_answer: [
+"2007",
+"1984",
+"2009"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What does TIE stand for in reference to the TIE Fighter in &quot;Star Wars&quot;?",
+correct_answer: "Twin Ion Engine",
+incorrect_answer: [
+"Twin Iron Engine",
+"Twin Intercepter Engine",
+"Twin Inception Engine"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Which iconic character is featured in movies such as &quot;The Enforcer&quot;, &quot;Sudden Impact&quot;, and &quot;The Dead Pool&quot;?",
+correct_answer: "Dirty Harry",
+incorrect_answer: [
+"Indiana Jones",
+"James Bond",
+"Harry Potter"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What is the name of the villian in the 2015 Russian-American Sci-Fi Movie &quot;Hardcore Henry&quot;?",
+correct_answer: "Akan",
+incorrect_answer: [
+"Estelle",
+"Jimmy",
+"Henry"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In the 1979 British film &quot;Quadrophenia&quot; what is the name of the seaside city the mods are visiting?",
+correct_answer: "Brighton",
+incorrect_answer: [
+"Eastbourne",
+"Mousehole",
+"Bridlington"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who played Baron Victor Frankestein in the 1957 Hammer horror film &quot;The Curse of Frankenstein&quot;?",
+correct_answer: "Peter Cushing",
+incorrect_answer: [
+"Boris Karloff",
+"Vincent Price",
+"Lon Chaney Jr."
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In the 2010 Nightmare on Elm Street reboot, who played Freddy Kruger?",
+correct_answer: "Jackie Earle Haley",
+incorrect_answer: [
+"Tyler Mane",
+"Derek Mears",
+"Gunnar Hansen"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Which one of these actors is said to be cut from the film &#039;E.T. the Extra-Terrestrial&#039;?",
+correct_answer: "Harrison Ford",
+incorrect_answer: [
+"Michael J. Fox",
+"Andy Kaufman",
+"Arnold Schwarzenegger"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What is the correct phrase for the Latin saying &quot;Romanes Eunt Domus&quot; in Monty Python&#039;s Life of Brian?",
+correct_answer: "Romani Ite Domum",
+incorrect_answer: [
+"Romans Go Home",
+"Roxani Ite Domum",
+"Tomate Ite Domum"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who played Sgt. Gordon Elias in &#039;Platoon&#039; (1986)?",
+correct_answer: "Willem Dafoe",
+incorrect_answer: [
+"Charlie Sheen",
+"Matt Damon",
+"Johnny Depp"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who voices the main character Blu in the 2011 animated film &quot;Rio&quot;?",
+correct_answer: "Jesse Eisenberg",
+incorrect_answer: [
+"Michael Cera",
+"Jonah Hill",
+"Zach Galifianakis"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In what year was the movie &quot;Police Academy&quot; released?",
+correct_answer: "1984",
+incorrect_answer: [
+"1986",
+"1985",
+"1983"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In Back to the Future Part II, Marty and Dr. Emmett Brown go to which future date?",
+correct_answer: "October 21, 2015",
+incorrect_answer: [
+"August 28, 2015",
+"July 20, 2015",
+"January 25, 2015"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In the 1999 movie Fight Club, which of these is not a rule of the &quot;fight club&quot;?",
+correct_answer: "Always wear a shirt",
+incorrect_answer: [
+"You do not talk about FIGHT CLUB",
+"Only two guys to a fight",
+"Fights will go on as long as they have to"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Darth Vader&#039;s famous reveal to Luke is iconic. But which of these is the right one?",
+correct_answer: "&quot;No. I am your father.&quot;",
+incorrect_answer: [
+"&quot;Luke, I am your father.&quot;",
+"&quot;You&#039;re wrong. I am your father.&quot;",
+"&quot;The truth is that I am your father.&quot;"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "This trope refers to minor characters that are killed off to show how a monster works.",
+correct_answer: "Red Shirt",
+incorrect_answer: [
+"Minions",
+"Expendables",
+"Cannon Fodder"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What year did the James Cameron film &quot;Titanic&quot; come out in theaters?",
+correct_answer: "1997",
+incorrect_answer: [
+"1996",
+"1998",
+"1999"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What city did the monster attack in the film, &quot;Cloverfield&quot;?",
+correct_answer: "New York, New York",
+incorrect_answer: [
+"Las Vegas, Nevada",
+"Chicago, Illinois",
+"Orlando, Florida"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What was the name of the protagonist in the movie Commando (1985)?",
+correct_answer: "John Matrix",
+incorrect_answer: [
+"Ben Richards",
+"Douglas Quaid",
+"Harry Tasker"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What is the correct spelling of the protagonist of the book in The NeverEnding Story (1984)?",
+correct_answer: "Atreyu",
+incorrect_answer: [
+"Atrayu",
+"Atraiyu",
+"Atraeyu"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In which movie does Robin Williams&#039; character have to disguise themselves into a woman?",
+correct_answer: "Mrs. Doubtfire",
+incorrect_answer: [
+"Old Dogs",
+"Jumanji",
+"Awakenings"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What type of cheese, loved by Wallace and Gromit, had it&#039;s sale prices rise after their successful short films?",
+correct_answer: "Wensleydale",
+incorrect_answer: [
+"Cheddar",
+"Moon Cheese",
+"Edam"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Which famous singer was portrayed by actor Kevin Spacey in the 2004 biographical film &quot;Beyond the Sea&quot;?",
+correct_answer: "Bobby Darin",
+incorrect_answer: [
+"Louis Armstrong",
+"Frank Sinatra",
+"Dean Martin"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who is the director of the 1991 film &quot;Silence of the Lambs&quot;?",
+correct_answer: "Jonathan Demme",
+incorrect_answer: [
+"Stanley Kubrick",
+"Frank Darabont",
+"Michael Bay"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who was the director of &quot;Scott Pilgrim vs. the World (2010)&quot;?",
+correct_answer: "Edgar Wright",
+incorrect_answer: [
+"Phil Lord",
+"Chris Miller",
+"Seth Rogan"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Leonardo Di Caprio won his first Best Actor Oscar for his performance in which film?",
+correct_answer: "The Revenant",
+incorrect_answer: [
+"The Wolf Of Wall Street",
+"Shutter Island",
+"Inception"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Which documentary film maker wrote and starred in the film &quot;My Scientology Movie&quot; which first debuted in 2015?",
+correct_answer: "Louis Theroux",
+incorrect_answer: [
+"Errol Morris",
+"Joe Berlinger",
+"Adam Curtis"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "What is the make and model of the tour vehicles in &quot;Jurassic Park&quot;?",
+correct_answer: "1992 Ford Explorer XLT",
+incorrect_answer: [
+"1992 Toyota Land Cruiser",
+"1992 Jeep Wrangler YJ Sahar",
+"Mercedes M-Class"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In &quot;Jurassic World&quot;, which company purchases InGen and creates Jurassic World?",
+correct_answer: "Masrani Global Corporation ",
+incorrect_answer: [
+"Biology Synthetics Technologies",
+"International Genetics Incorporated",
+"International Genetic Technologies"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who directed the movie &quot;Alien&quot;?",
+correct_answer: "Ridley Scott",
+incorrect_answer: [
+"Christopher Nolan",
+"Michael Bay",
+"James Cameron"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who plays &quot;Bruce Wayne&quot; in the 2008 movie &quot;The Dark Knight&quot;?",
+correct_answer: "Christian Bale",
+incorrect_answer: [
+"Michael Caine",
+"Ron Dean",
+"Heath Ledger"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Which former Star Trek actor directed Three Men and a Baby (1987)?",
+correct_answer: "Leonard Nimoy",
+incorrect_answer: [
+"William Shatner",
+"George Takei",
+"James Doohan"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Which town is the setting for the Disney movie The Love Bug (1968)?",
+correct_answer: "San Francisco",
+incorrect_answer: [
+"Los Angeles",
+"Sacramento",
+"San Jose"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "Who provided a majority of the songs and lyrics for &quot;Spirit: Stallion of the Cimarron&quot;?",
+correct_answer: "Bryan Adams",
+incorrect_answer: [
+"Smash Mouth",
+"Oasis",
+"Air Supply"
+]
+},
+{
+category: "Entertainment: Film",
+type: "multiple",
+difficulty: "medium",
+question: "In the Friday The 13th series, what is Jason&#039;s mother&#039;s first name?",
+correct_answer: "Pamela",
+incorrect_answer: [
+"Mary",
+"Christine",
+"Angeline"
+]
+},
 ]);
